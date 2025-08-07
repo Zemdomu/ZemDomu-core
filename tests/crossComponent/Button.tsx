8e3410382406f12dcd029eaaeec38c7a29a5f6d5
Zacharias Eryd Berlin <zacharias.erydberlin@additude.se>
2025-08-07 10:29:37 +0200

    wIP: Added test components

---
+++ tests/crossComponent/Button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export default function Button() {
+  return (
+    <div>
+      <h1>Button</h1>
+      <h4>Subsection</h4>
+    </div>
+  );
+}
---
+++ tests/crossComponent/Page.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Button from "./Button";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Hello</h1>
+      <h2>Intro</h2>
+      <Button />
+    </main>
+  );
+}
